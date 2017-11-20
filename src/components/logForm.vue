92cf12b50b3691da08481746b7230989e289abe9
y5621857 <229655153@qq.com>
2017-11-20 20:38:25 +0800

    step one ，indexPage 页面完善

---
+++ src/components/logForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tpl',
+    data() {
+      return {
+        msg: 'this is a tpl'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
